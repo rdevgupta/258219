f95a6839524823f412c800889e5625c1021e3775
Dev Gupta <guptadev359@gmail.com>
2021-04-18 22:17:18 +0530

    added one more code

---
+++ find_runner_up.py
@@ -0,0 +1,10 @@
+print("Enter the inputs: ")
+n = int(input())
+arr = map(int, input().split())
+arr = list(arr)
+arr.sort(reverse = True)
+for i in range(1, n):
+    if(arr[0] == arr[i]):
+        continue
+    print(arr[i])
+    break
