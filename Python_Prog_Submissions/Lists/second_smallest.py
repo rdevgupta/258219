999b01255c0d87975ea7550bce2e05ba11145fa2
Dev Gupta <guptadev359@gmail.com>
2021-04-23 00:33:01 +0530

    list code #2

---
+++ Python_Prog_Submissions/Lists/second_smallest.py
@@ -0,0 +1,6 @@
+#Write a Python program to find the second smallest number in a list
+
+List = [1,5,8,2,3,9,4,2,0]
+#sorts in ascending order by default
+List.sort()
+print("Second smallest element in list is: %d" %List[1])
